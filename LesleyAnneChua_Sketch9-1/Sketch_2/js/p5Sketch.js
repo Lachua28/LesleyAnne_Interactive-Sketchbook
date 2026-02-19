9a61eba9ad5be480d2c901055083f86ac902a9bb
Lesley Anne C. <lchua@sfsu.edu>
2026-02-19 13:44:24 -0800

    Add files via upload

---
+++ LesleyAnneChua_Sketch9-1/Sketch_2/js/p5Sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas (600, 400);
+    //accumulation
+    background(74, 58, 53);
+}
+
+function draw() {
+    noStroke();
+    // direct manipulation
+    ellipse(mouseX, mouseY, 40)
+}
